31e193d3ec12436c16ac76b6c34467abbbdbfd2c
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-03-27 03:44:14 +0100

    fix: only execute code with __main__

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,20 +3,22 @@
 from pymongo import MongoClient
 from pprint import pprint as pp
 
-client = MongoClient()
-nginx = client.logs.nginx
-# pp(list(nginx.find(limit=3)))
-print(f'{nginx.count_documents({})} logs')
-print('Methods:')
 
-methods = {
-    'GET': nginx.count_documents({'method': 'GET'}),
-    'POST': nginx.count_documents({'method': 'POST'}),
-    'PUT': nginx.count_documents({'method': 'PUT'}),
-    'PATCH': nginx.count_documents({'method': 'PATCH'}),
-    'DELETE': nginx.count_documents({'method': 'DELETE'})
-}
-for method, count in methods.items():
-    print(f'    method {method}: {count}')
+if __name__ == '__main__':
+    client = MongoClient()
+    nginx = client.logs.nginx
+    # pp(list(nginx.find(limit=3)))
+    print(f'{nginx.count_documents({})} logs')
+    print('Methods:')
 
-print(f"{nginx.count_documents({'path': '/status'})} status check")
+    methods = {
+        'GET': nginx.count_documents({'method': 'GET'}),
+        'POST': nginx.count_documents({'method': 'POST'}),
+        'PUT': nginx.count_documents({'method': 'PUT'}),
+        'PATCH': nginx.count_documents({'method': 'PATCH'}),
+        'DELETE': nginx.count_documents({'method': 'DELETE'})
+    }
+    for method, count in methods.items():
+        print(f'    method {method}: {count}')
+
+    print(f"{nginx.count_documents({'path': '/status'})} status check")
