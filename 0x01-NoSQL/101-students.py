3c5b03205f7ed54311a427ff8268996628da4e7e
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-03-27 05:23:14 +0100

    Try computing top students

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Definition of top_students function """
+
+
+def top_students(mongo_collection):
+    """
+    Returns all students sorted by average score
+
+    Args:
+        mongo_collection (obj): The `pymongo` collection object
+
+    Returns:
+        list: A list of all students documents sorted by average score
+    """
+    
+    return []
