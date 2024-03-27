fde15fa402f5211d38d954b126a957362d682c54
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-03-27 01:40:48 +0100

    Correct the shebang and the code indentation

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Definition of list_all function
 """
 
 
 def list_all(mongo_collection):
-  """List all documents in a collection
-  """
-  result = mongo_collection.find()
-  if result:
-    return list(result)
-  
-  return []
+    """List all documents in a collection
+    """
+    result = mongo_collection.find()
+    if result:
+        return list(result)
+
+    return []
