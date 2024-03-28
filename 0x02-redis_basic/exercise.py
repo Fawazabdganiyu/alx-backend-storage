75e96f102269833b8a13e3eb314097ccae61e032
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-03-28 05:58:21 +0100

    Write strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
-"""
-"""
+""" Definition of Cache module """
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """ Definition of Cache class """
+
+
+def __init__(self):
+    """ Instantiation """
+    self._redis = redis.Redis()
+    self._redis.flushdb()
+
+
+def store(self, data: Union[str, bytes, int, float]) -> str:
+    """ Store """
+    key = str(uuid.uuid4())
+    self._redis.set(key, data)
+
+    return key
