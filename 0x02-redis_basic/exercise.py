50bc3836bf431bbd92f306b53ebfcda059337c67
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-03-29 15:10:48 +0100

    fix: set the defined class methods properly

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,16 +8,14 @@ from typing import Union
 class Cache:
     """ Definition of Cache class """
 
+    def __init__(self):
+        """ Instantiation """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
-def __init__(self):
-    """ Instantiation """
-    self._redis = redis.Redis()
-    self._redis.flushdb()
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ Store """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
 
-
-def store(self, data: Union[str, bytes, int, float]) -> str:
-    """ Store """
-    key = str(uuid.uuid4())
-    self._redis.set(key, data)
-
-    return key
+        return key
